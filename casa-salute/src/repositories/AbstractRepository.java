62fed4cd77ac0b8985773d3d51fda6c76285749c
manuel-de-gregori <manueldegregori@gmail.com>
2024-07-27 16:17:16 +0200

    developed repositories

--- casa-salute/src/repositories/AbstractRepository.java
+++ casa-salute/src/repositories/AbstractRepository.java
@@ -43,6 +43,7 @@ public abstract class AbstractRepository<T extends UniqueResource> {
         }
 
         // lazy update:
+        data.setId(id);
         Remove(id);
         Add(id, data);
     }
